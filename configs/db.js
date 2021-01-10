58cd14f8152bb61daae050de38e5ee2af8d74fac
Daniel F. Delgado <danidelgadok@gmail.com>
2021-01-10 10:13:53 -0500

    Added basic web server

---
+++ configs/db.js
@@ -0,0 +1,8 @@
+// DATABASE SETTIGS
+module.exports = {
+   url: 'mongodb://127.0.0.1:27017/lae',
+   options: {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+   }
+} 
---
+++ index.js
@@ -0,0 +1,30 @@
+// IMPORT LIBRARIES
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// IMPORT CONFIGS
+const db = require('./configs/db')
+
+// MONGOOSE SETTINGS
+mongoose.connect(db.url, db.options)
+
+// SERVER SETTINGS
+const port = 8080
+const app = express()
+
+// EXPRESS SETTINGS
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// EXPRESS ROUTES
+// require('./routes/user')(app)
+
+// RUN SERVER
+app.listen(port, () => {
+  console.log('Server is now running at port: ', port)
+})
